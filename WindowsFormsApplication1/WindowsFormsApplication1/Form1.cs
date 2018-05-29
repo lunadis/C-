0dbad3929423c4a6e8da517d9676202a5c3984d5
Luis Lopes <luislopes116@outlook.com>
2018-05-29 20:20:30 -0300

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmTeste1 tst1 = new frmTeste1();
+            tst1.MdiParent = this;
+            tst1.Show();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblData.Text = "Data:" + DateTime.Now.ToString("dd/mm/yyyy");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = "hora:" + DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/frmTeste1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmTeste1 : Form
+    {
+        public frmTeste1()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTeste1_Load(object sender, EventArgs e)
+        { 
+        }
+
+        private void frmTeste1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("deseja sair?", "titulo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) MessageBox.Show("saindo");
+            else
+            {
+                MessageBox.Show("ficando");
+                e.Cancel = true;
+            }
+        }
+    }
+}
